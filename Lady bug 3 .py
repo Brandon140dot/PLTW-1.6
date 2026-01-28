10e7bcb6a471791b79533014afd046e0769688e8
Brandon140dot <bavatarmagic1@gmail.com>
2026-01-28 10:55:18 -0500

    Create Lady bug 3.py for turtle graphics
    Added a new turtle graphics script to draw a ladybug.

---
+++ Lady bug 3 .py
@@ -0,0 +1,19 @@
+import turtle as trtl
+
+ladybug = trtl.Turtle()
+ladybug.speed(0)
+ladybug.pensize(40)
+
+ladybug.color("black")
+ladybug.circle(5)
+
+ladybug.pensize(40)
+ladybug.color("black")
+ladybug.circle(5)
+
+ladybug.penup()
+ladybug.goto(0, -55)
+ladybug.pendown()
+ladybug.color("red")
+ladybug.pensize(40)
+ladybug.circle(20)
