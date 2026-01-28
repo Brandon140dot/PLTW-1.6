2539c89b4a943a2edee95e46cd9cf5ae54aaad43
Brandon140dot <bavatarmagic1@gmail.com>
2026-01-28 10:56:01 -0500

    Create Lady bug 4.py for drawing a ladybug
    Add a turtle graphics program to draw a ladybug.

---
+++ Lady bug 4 .py
@@ -0,0 +1,27 @@
+import turtle as trtl
+
+ladybug = trtl.Turtle()
+ladybug.speed(0)
+ladybug.pensize(40)
+
+ladybug.color("black")
+ladybug.circle(5)
+
+ladybug.pensize(40)
+ladybug.color("black")
+ladybug.circle(5)
+
+ladybug.penup()
+ladybug.goto(0, -55)
+ladybug.pendown()
+ladybug.color("red")
+ladybug.pensize(40)
+ladybug.circle(20)
+
+ladybug.penup()
+ladybug.goto(0, 5)
+ladybug.setheading(270)
+ladybug.pendown()
+ladybug.color("black")
+ladybug.pensize(2)
+ladybug.forward(75)
